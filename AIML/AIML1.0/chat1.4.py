1c6ce619bb3b805cf05efee6bb5a3c53191fe52a
Wang Xiaobo <1299784636@qq.com>
2019-09-07 16:57:31 +0800

    Initial commit

---
+++ AIML/AIML1.0/chat1.4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import aiml
+import os
+
+os.chdir('./resource')
+mybot = aiml.Kernel()
+mybot.learn("startup.xml")
+mybot.respond('load nao')
+
+#处理响应
+while True:
+    message = input("Enter your message >> ")
+    if("exit" == message):
+        exit()
+    response = mybot.respond(message) # 机器人应答
+    if(response):
+        print(response)
+    else:
+        print("我没听清，可以再说一次吗？或者尝试换一种提问方式")
